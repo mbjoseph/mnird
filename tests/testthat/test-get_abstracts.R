5735a73db9459b34f985a8ef4b3e6fc2035bb108
Max Joseph <maxwellbjoseph@gmail.com>
2019-10-25 21:45:45 -0600

    Use vcr

--- tests/testthat/test-get_abstracts.R
+++ tests/testthat/test-get_abstracts.R
@@ -2,6 +2,8 @@ test_that("Abstracts are returned, given a bibfile", {
   test_bibfile <- system.file("extdata",
                               "neon-biodiversity-instrumentation-ml.bib",
                               package = "mnird")
-  abstracts <- get_abstracts(test_bibfile)
+  vcr::use_cassette("abstracts", {
+    abstracts <- get_abstracts(test_bibfile)
+  })
   expect_s3_class(abstracts, "tbl_df")
 })
--- tests/testthat/test-get_document_terms.R
+++ tests/testthat/test-get_document_terms.R
@@ -1,8 +1,10 @@
 test_that("get_document_terms returns a TermDocumentMatrix", {
-  abstract_df <- system.file("extdata",
-                             "neon-biodiversity-instrumentation-ml.bib",
-                             package = "mnird") %>%
-    get_abstracts()
+  test_bibfile <- system.file("extdata",
+                              "neon-biodiversity-instrumentation-ml.bib",
+                              package = "mnird")
+  vcr::use_cassette("abstracts", {
+    abstract_df <- get_abstracts(test_bibfile)
+  })
   dtm <- get_document_terms(abstract_df)
   expect_s3_class(dtm, "DocumentTermMatrix")
 })
